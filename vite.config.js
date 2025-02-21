76628c1555274bb3f54890549fe919d7ac50ab0b
felix <felix@micous.com>
2025-02-21 15:14:27 +0800

    fix: 默认端口的调整

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/child_ai_h5/',
   plugins: [react()],
   server: {
     port: 3001
